299f9d773f87c344f294be3533f72dce6b1569e4
ibilim <73077940+ibilim@users.noreply.github.com>
2021-06-18 17:16:01 +0200

    Update find_data.py

--- find_data.py
+++ find_data.py
@@ -1,26 +1,73 @@
-import json as js
-import nltk
-import pandas as pd
-fh=open('2f392438-e215-4169-bebf-21ac4ff253e1.json','r')
-fh_1=js.load(fh)
-
-text1=fh_1
+# 1.st Approach:
+# Extract all possible sentences that include data then run topic analysis
+from nltk.corpus import stopwords
+import gensim
+from gensim import corpora, models
+from nltk import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import RegexpTokenizer
+import re
+from gensim.models.ldamodel import LdaModel
+#load text in json format
+f_hand=open('2100032a-7c33-4bff-97ef-690822c43466.json','r')
+text=js.load(f_hand)
+#convert json to string
 full_text=''
-for i in range(0,len(text1)):
-    full_text=full_text+text1[i]['text']
-full_text
-
-data_words=['survey','study']
+for i in range(0,len(text)):
+    full_text=full_text+' '+text[i]['text']
+#data Sentences 
+data_words=['Study','Survey','Statistics','Data','Dataset','Database','census','Census']
 sentences=nltk.sent_tokenize(full_text)
+data_sentences=[w for w in sentences for i in range(0,len(data_words)) if data_words[i] in w]
 
-data_sentences=[w for w in sentences if data_words[1] in w.lower() ]
-data_sentences
+#free from punctuation and pharantheses
+tokenizer=RegexpTokenizer(r'\w+')
+free_from_space=tokenizer.tokenize(' '.join(data_sentences))
+#tokenized text
+tokenized_text=nltk.word_tokenize(' '.join(free_from_space))
+# normalization
+norm_text=' '.join(tokenized_text).lower()
 
-#Noun-Phrase Approach to detect datasources
-import re,pprint
-sentence1=nltk.pos_tag(nltk.word_tokenize(data_sentences[0]),lang='eng')
-grammar = "NP: {<DT>?<NNP>}"
-cp=nltk.RegexpParser(grammar)
-result=cp.parse(sentence1)
-print(result)
-#result.draw()
+#Lemmatization
+lemmatizer=nltk.WordNetLemmatizer()
+lemmatized_text=[lemmatizer.lemmatize(w) for w in norm_text.split()]
+#stopword_free text
+stopwrds=stopwords.words('English')
+stopword_free=[w for w in nltk.word_tokenize(' '.join(lemmatized_text)) if w not in stopwrds]
+dictionary=corpora.Dictionary([stopword_free])
+corpus=[dictionary.doc2bow(item) for item in [stopword_free]]
+ldamodel=gensim.models.ldamodel.LdaModel(corpus,num_topics=1, id2word=dictionary,passes=25,per_word_topics=True)
+ldamodel.print_topics(num_topics=1,num_words=5)[0][1]
+
+#2Approach to detect datasources: Countvectorizer with n-grams and 
+import nltk,re
+import gensim
+import json as js
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from gensim.models.ldamodel import LdaModel
+#load text in json format
+f_hand=open('2100032a-7c33-4bff-97ef-690822c43466.json','r')
+text=js.load(f_hand)
+#convert json to string
+full_text=''
+for i in range(0,len(text)):
+    full_text=full_text+' '+text[i]['text']
+
+# Eliminate stopwords 
+free_from_stop=[w for w in nltk.word_tokenize(full_text) if w not in stopwords.words('English')]
+# extract data sentences form text
+data_words=['Study','Survey','Statistics','Dataset','Database','data','census','Census']
+sentences=nltk.sent_tokenize(' '.join(free_from_stop))
+data_sentences=[w for w in sentences for i in range(0,len(data_words)) if data_words[i] in w]
+#print(data_sentences)
+#countvectorizer and fit and transform text 
+vectorizer=CountVectorizer(analyzer='word',ngram_range=(3,5))
+fitted_model=vectorizer.fit_transform(data_sentences)
+# create corpus to use in Lda model
+corpus = gensim.matutils.Sparse2Corpus(fitted_model, documents_columns=False)
+id_map = dict((v, k) for k, v in vectorizer.vocabulary_.items())
+
+# Lda model
+ldamodel=LdaModel(corpus,num_topics=1,id2word=id_map,passes=50,per_word_topics=True)
+ldamodel.print_topics(num_topics=1,num_words=5)
